91f2b5234660897413821a718842913e09e0a68e
MANNE UDAY KIRAN <143863214+anonymous200443@users.noreply.github.com>
2025-04-19 17:58:16 +0530

    Update api.js

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api/students'
+  baseURL: 'https://student-management-system-1cv3.onrender.com/api/students'
 });
 
 export default api;
